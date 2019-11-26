487181b1dfb323758d9a941e34f18bad6793ee1d
okas832 <okas832@kaist.ac.kr>
2019-11-26 16:38:16 +0900

    Implement small part of yacc

---
+++ parser/c_yacc.py
@@ -0,0 +1,86 @@
+import c_lex
+import ply.yacc as yacc
+
+tokens = c_lex.tokens
+
+def p_goal_1(p):
+    "goal : def"
+    p[0] = [p[1]]
+
+def p_goal_2(p):
+    "goal : def goal"
+    p[0] = [p[1]] + p[2]
+
+def p_def_1(p):
+    "def : type def_many"
+    p[0] = [p[1], p[2]]
+
+def p_def_2(p):
+    "def : type ID LPAREN args LBRACE body"
+    p[0] = (p[1], p[2], p[4], p[6])
+
+def p_args_1(p):
+    "args : arg RPAREN"
+    p[0] = [p[1]]
+
+def p_args_2(p):
+    "args : arg COMMA args"
+    p[0] = [p[1]] + p[3]
+
+def p_arg(p):
+    "arg : type ID"
+    p[0] = (p[1], p[2])
+
+def p_body_1(p):
+    "body : RBRACE"
+    p[0] = None
+
+def p_def_many_1(p):
+    "def_many : ID SEMICOL"
+    p[0] = [(p[1], None)]
+
+def p_def_many_2(p):
+    "def_many : ID assign SEMICOL"
+    p[0] = [(p[1], p[2])]
+
+def p_def_many_3(p):
+    "def_many : ID COMMA def_many"
+    p[0] = [(p[1], None)] + p[3]
+
+def p_def_many_4(p):
+    "def_many : ID assign COMMA def_many"
+    p[0] = [(p[1], p[2])] + p[4]
+
+def p_assign_1(p):
+    "assign : ASSIGN const"
+    p[0] = p[2]
+
+def p_assign_2(p):
+    "assign : ASSIGN ID"
+    p[0] = p[2]
+
+def p_type(p):
+    """type : INT
+            | FLOAT
+            | VOID"""
+    p[0] = p[1]
+
+
+def p_const(p):
+    """const : IVAL
+             | FVAL
+             | SVAL
+             | CVAL"""
+    p[0] = p[1]
+
+yacc.yacc()
+if __name__ == "__main__":
+    with open("input.c", "r") as f:
+        result = yacc.parse(f.read())
+    print(result)
+
+
+
+
+
+
