412ee5c96e3e123c474cf89b1817354b1e4894be
okas832 <okas832@kaist.ac.kr>
2019-12-05 21:33:51 +0900

    Improve variable declaration

--- parser/c_yacc.py
+++ parser/c_yacc.py
@@ -14,36 +14,57 @@ def p_goal_2(p):
     "goal : def goal"
     p[0] = GOAL([p[1]]) + p[2]
 
+def p_def_1(p):
+    "def : defv SEMICOL"
+    p[0] = p[1]
+
+def p_def_2(p):
+    "def : deff"
+    p[0] = p[1]
 
 # type id [=value] *[,id[=value]];;
 
-def p_def_1(p):
-    "def : type def_many"
+def p_defv_1(p):
+    "defv : type defv_many"
     p[0] = VDEF(p[1], p[2])
 
-def p_def_many_1(p):
-    "def_many : ID SEMICOL"
+def p_defv_many_1(p):
+    "defv_many : id_adv array"
     p[0] = [(p[1], None)]
 
-def p_def_many_2(p):
-    "def_many : ID assign SEMICOL"
+def p_defv_many_2(p):
+    "defv_many : id_adv assign"
     p[0] = [(p[1], p[2])]
 
-def p_def_many_3(p):
-    "def_many : ID COMMA def_many"
+def p_defv_many_3(p):
+    "defv_many : id_adv array COMMA defv_many"
     p[0] = [(p[1], None)] + p[3]
 
-def p_def_many_4(p):
-    "def_many : ID assign COMMA def_many"
+def p_defv_many_4(p):
+    "defv_many : id_adv assign COMMA defv_many"
     p[0] = [(p[1], p[2])] + p[4]
 
+def p_id_adv_1(p):
+    "id_adv : ID"
+    p[0] = p[1]
+
+def p_id_adv_2(p):
+    "id_adv : TIMES id_adv"
+    p[0] = p[2]
+
+def p_array_1(p):
+    "array : empty"
+    pass
+
+def p_array_2(p):
+    "array : LBRACK IVAL RBRACK"
+    pass
 
 # type id([arg] *[,arg]){*[expr]}
 
-def p_def_2(p):
-    "def : type ID LPAREN s_args RPAREN LBRACE body RBRACE"
+def p_deff(p):
+    "deff : type ID LPAREN s_args RPAREN LBRACE body RBRACE"
     p[0] = FDEF(p[1], p[2], p[4], p[7])
-
 
 # for special case(no argument)
 def p_s_args_1(p):
@@ -102,32 +123,40 @@ def p_stmt_2(p):
 
 # WHILE
 def p_stmt_3(p):
-    "stmt : WHILE LPAREN expr RPAREN stmt"
+    "stmt : WHILE LPAREN ndexpr RPAREN stmt"
     pass
 
 # FOR
 def p_stmt_4(p):
-    "stmt : FOR LPAREN expr SEMICOL expr SEMICOL expr RPAREN stmt"
+    "stmt : FOR LPAREN expr SEMICOL ndexpr SEMICOL ndexpr RPAREN stmt"
     pass
 
 # COND, IF
 def p_stmt_5(p):
-    "stmt : IF LPAREN expr RPAREN stmt"
+    "stmt : IF LPAREN ndexpr RPAREN stmt"
     pass
 
 # COND, IF - ELSE
 def p_stmt_6(p):
-    "stmt : IF LPAREN expr RPAREN stmt ELSE stmt"
+    "stmt : IF LPAREN ndexpr RPAREN stmt ELSE stmt"
     pass
 
+def p_expr(p):
+    """expr : ndexpr
+            | defv"""
+    pass
 
+# expr s.t. no define variable
+def p_ndexpr_1(p):
+    """ndexpr : const"""
+    pass
+
+def p_ndexpr_2(p):
+    """ndexpr : ID"""
+    pass
 
 def p_assign_1(p):
-    "assign : ASSIGN const"
-    p[0] = p[2]
-
-def p_assign_2(p):
-    "assign : ASSIGN ID"
+    "assign : ASSIGN ndexpr"
     p[0] = p[2]
 
 def p_type(p):
