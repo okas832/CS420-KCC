8c2bd54e21b794cb3ded40b31da6dc0e25e06fa8
ironore15 <alex8757@kaist.ac.kr>
2019-12-08 14:21:56 +0000

    INTERPRET: Implement VALUE class with type casting

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -3,23 +3,24 @@ from ctype import *
 
 
 class VALUE(object):
-    def __init__(self, addr, value, ctype):
+    def __init__(self, value, ctype):
         assert isinstance(ctype, CType)
-        self.addr = addr
-        self.value = value
+        if ctype == TFloat():
+            self.value = float(value)
+        elif ctype == TInt():
+            value = int(value) & 0xFFFFFFFF
+            self.value = value if value < 0x80000000 else value - 0x100000000
+        elif ctype == TChar():
+            value = int(result) & 0xFF
+            self.value = value if value < 0x80 else value - 0x100
+        else:
+            raise ValueError("Not Implemented Type '%s'" % ctype)
         self.ctype = ctype
 
     def __repr__(self):
         if isinstance(self.ctype, TVoid):
             return "(void)"
-        elif isinstance(self.ctype, TInt):
-            return "(int) %d" % self.value
-        elif isinstance(self.ctype, TFloat):
-            return "(float) %f" % self.value
-        elif isinstance(self.ctype, TChar):
-            return "(char) %d" % self.value
-
-        raise TypeError("Not Implemented")
+        return "(%s) %d" % (self.ctype, self.value)
 
 
 def id_resolve(expr, genv, lenv):
@@ -42,11 +43,30 @@ def define_var(expr):
     assert isinstance(expr, VDEFID)
 
 
-def calc_binop(expr, genv, lenv):
+def exec_cast(expr, genv, lenv):
+    result = exec_expr(expr.expr, genv, lenv)
+
+    if isinstance(expr, C2I):
+        assert result.ctype == TChar()
+        return VALUE(result.value, TInt())
+    elif isinstance(expr, I2C):
+        assert result.ctype == TInt()
+        return VALUE(result.value, TChar())
+    elif isinstance(expr, I2F):
+        assert result.ctype == TInt()
+        return VALUE(result.value, TFloat())
+    elif isinstance(expr, F2I):
+        assert result.ctype == TFloat()
+        return VALUE(result.value, TInt())
+
+    raise ValueError("Not Implemented Type Casting: %s" % result.expr)
+
+
+def exec_binop(expr, genv, lenv):
     assert isinstance(expr, BINOP)
 
-    lhs = calc_expr(expr.lhs)
-    rhs = calc_expr(expr.rhs)
+    lhs = exec_expr(expr.lhs)
+    rhs = exec_expr(expr.rhs)
     assert lhs.ctype == rhs.ctype
     ctype = lhs.ctype
 
@@ -81,15 +101,17 @@ def calc_binop(expr, genv, lenv):
         result = lhs.value == rhs.value
     elif expr.op == "!=":
         result = lhs.value != rhs.value
+    else:
+        raise ValueError("Not Implemented Operator '%s'" % expr.op)
+
+    return VALUE(result, ctype)
 
 
-    return VALUE()
-
-
-def calc_expr(expr, genv, lenv):
+def exec_expr(expr, genv, lenv):
     if isinstance(expr, ID):
         return id_resolve(expr, genv, lenv)
-    elif isinstance(expr, 
+    elif isinstance(expr, TEXPR):
+        return exec_cast(expr, genv, lenv)
     elif isinstance(expr, PREOP):
         pass
     elif isinstance(expr, POSTOP):
