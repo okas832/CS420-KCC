a71545631af7d9ccd3c5d6a1f105c30ddcd69e21
jehoon315 <54788546+jehoon315@users.noreply.github.com>
2019-12-22 17:56:34 +0900

    add some sample input c code
    sum : summing 0 to 10
    gcd : calculating greatest common divisor

---
+++ sample_input/sum.c
@@ -0,0 +1,37 @@
+float sum(int length, int *value){
+	int i;
+	float total;
+	total = 0;
+
+	for(i = 0; i < length; i++){
+		total = total + value[i];
+	}
+
+	return total;
+}
+
+int main(void){
+	int count, i;
+	int record[10];
+	float total;
+
+	count = 11;
+	total = 0;
+	
+	printf("record :");
+
+	for(i = 0; i < count; i++) {
+		record[i] = i;
+		printf(" %d", record[i]);
+	}
+
+	printf("\n\nCalculating...\n");
+
+	for(i = 1; i <= count; i++) {
+		total = sum(i, record);
+		printf("%f\n", total);
+	}
+
+	printf("Done!\n");
+
+}
