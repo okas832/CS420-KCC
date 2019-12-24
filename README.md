477efb628d94d2376077280ffffe0bf5705c8d5e
SeungHyun Lee <xion@kaist.ac.kr>
2019-12-25 01:39:28 +0900

    Merge branch 'console' into interpreter

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # KCC (KAIST Compiler Collection)
 This is a project for CS420(Compiler Design) in KAIST.
-
--- console.py
+++ console.py
@@ -1,87 +1,58 @@
 import sys
-from logger import logger
+from logger import logger as Logg
+from interpreter import interpreter as Interp
+import parser
 
-class consoleErr(Exception):
-    def __init__(self, _message):
-        self.message = _message
+class Console:
+    def __init__(self, filename):
+        self.filename = filename
 
-class Console():
-    def __init__(self, _filename):
-        self.filename = _filename
-
-    def load(self):
-        # load file to interpreter and initialize logger
+    def next(self):
+        ## todo : execute with interpreter and update with logger
         pass
     
-    def parseCommand(self, command):
-        args = command.split(' ')
+    def parseCommand(self, line):
+        args = line.split(' ')
         if args[0] == 'next':
             if len(args) <= 1:
-                # next(1)
-                pass
+                self.next()
             else:
                 n = int(args[1])
-                # next(n)
-                pass
-        elif args[0] == 'print':
+                for i in range(n):
+                    self.next()
+        elif args[0] == 'print' or args[0] == 'trace':
             if len(args) <= 1:
-                raise(consoleErr("Require second arg"))
+                print("Error: Require second arguemnt")
             else:
-                name = args[1]
-                # print(name)
-                pass
-        elif args[0] == 'trace':
-            if len(args) <= 1:
-                raise(consoleErr("Require second arg"))
-            else:
-                name = args[1]
-                # trace(name)
-                pass
+                Logg.command(args[0], args[1])
         elif args[0] == 'exit':
-            print("Terminating...")
-            return True
+            return False
         else:
-            raise(consoleErr("Invalid command"))
-        return False
+            print("Error: Invalid command")
+        return True
 
 def help():
-    # todo : print help message
-    print("### help message here###")
-
-def topmessage():
-    # todo : print top message
-    print("KCC : Mini-C Interpreter v0.0.1")
-    print("CS420 Project, Fall 2019, KAIST")
-    print("Main loop initiated")
+    print("# KCC - mini-C Compiler and Interpreter ###")
+    print("## Usage : python console.py <input file>")
+    print("## Interpreter Commands")
+    print("* next <count>")
+    print("* print <varname>")
+    print("* trace <varname>")
 
 def main():
-    argc = len(sys.argv)
-    # if no additional args, print help message
-    if argc <= 1:
+    if len(sys.argv) <= 1:
         help()
         return
-
-    # 1. read filename
     filename = sys.argv[1]
+    # 1. load file and initialize console
     console = Console(filename)
-    
-    # 2. load file
-    print("Loading file...")
-    console.load()
-    print("Load complete!")
-    # 2.1. top message 
-    topmessage()
-
-    # 3. start main loop
+    # 2. main loop
     while True:
-        try:
-            command = input("$ ")
-            exiting = console.parseCommand(command)
-            if exiting:
-                break
-        except consoleErr as e:
-            print("Error: " + e.message)
-            
+        comm = input("$ ")
+        loop = console.parseCommand(comm)
+        if not loop:
+            break
+    # 3. exit
 
 if __name__ == "__main__":
     main()
